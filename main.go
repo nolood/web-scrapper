264c2e370481e0c2294ae66b27cdb6ecb43db1f8
nolood <a9514548799@gmail.com>
2023-11-02 17:17:14 -0800

    feat: scrap all vacancies from all pages

--- main.go
+++ main.go
@@ -3,35 +3,64 @@ package main
 import (
 	"fmt"
 	"github.com/gocolly/colly/v2"
+	"github.com/joho/godotenv"
+	"log"
+	"strconv"
 	"strings"
+	"web-scrapper/pkg/postgres"
 )
+
+const baseUrl string = "https://chelyabinsk.hh.ru"
+
+func init() {
+	if err := godotenv.Load(); err != nil {
+		log.Print("No .env file found")
+	}
+	postgres.ConnectToDb()
+}
 
 func main() {
 	c := colly.NewCollector()
 	
-	var baseUrl string = "https://chelyabinsk.hh.ru"
+	var page int
+	var err error
 	
-	c.OnHTML("div.vacancy-serp-item__layout div.serp-item-controls a.bloko-button", func(element *colly.HTMLElement) {
-		var qString []string = strings.Split(element.Attr("href"), "=")
-		var vacancyId string = strings.Split(qString[1], "&")[0]
-		url := baseUrl + "/vacancy/" + vacancyId
-		vacancyC := colly.NewCollector()
-		
-		vacancyC.OnRequest(func(request *colly.Request) {
-			fmt.Println("Visiting", request.URL)
-		})
-		
-		vacancyC.OnHTML("span.bloko-header-section-2", func(element *colly.HTMLElement) {
-			fmt.Println(element.Text)
-		})
-		
-		vacancyC.Visit(url)
-		
+	c.OnHTML("div.vacancy-serp-item__layout div.serp-item-controls a.bloko-button", getVacancy)
+	
+	c.OnHTML("div.pager span.pager-item-not-in-short-range span.pager-item-not-in-short-range a", func(element *colly.HTMLElement) {
+		//fmt.Println("Visiting", element.Text)
+		page, err = strconv.Atoi(element.Text)
+		if err != nil {
+			panic(err)
+		}
 	})
-	
 	c.OnRequest(func(r *colly.Request) {
 		fmt.Println("Visiting", r.URL)
 	})
 	
-	c.Visit(baseUrl + "/search/vacancy?text=junior+front-end&area=113&page=0")
+	for i := 0; i <= page; i++ {
+		url := fmt.Sprintf("/search/vacancy?text=front-end&area=113&page=%d", i)
+		c.Visit(baseUrl + url)
+		
+		fmt.Println(url)
+	}
+}
+
+func getVacancy(element *colly.HTMLElement) {
+	var qString []string = strings.Split(element.Attr("href"), "=")
+	var vacancyId string = strings.Split(qString[1], "&")[0]
+	
+	isExist := postgres.IsVacancyExist(vacancyId)
+	vacancyC := colly.NewCollector()
+	url := baseUrl + "/vacancy/" + vacancyId
+	
+	if !isExist {
+		//postgres.AddVacancy(vacancyId)
+		vacancyC.OnHTML("span.bloko-header-section-2", func(element *colly.HTMLElement) {
+			//fmt.Println(element.Text)
+			fmt.Println(url)
+		})
+	}
+	
+	vacancyC.Visit(url)
 }
